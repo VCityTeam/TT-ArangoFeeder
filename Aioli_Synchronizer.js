75c5c0704c521db13ced8fcc9168ad36ccccdaca
vAbergel <116064860+vAbergel@users.noreply.github.com>
2024-03-28 12:09:36 +0100

    remove server listening

--- Aioli_Synchronizer.js
+++ Aioli_Synchronizer.js
@@ -848,23 +848,3 @@ async function loadAioliData(){
 
 }
 loadAioliData();
-
-// 0. Create server and listen
-var server = https.createServer(function (req, res) {
-
-    var url_parts = url.parse(req.url, true);
-    var name = url_parts.query.name;
-    if (name) {
-        console.log('Name: ' +name);
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({message: 'Hello ' +name + '!'}));
-    } else {
-        console.log('No name!');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.readFile('index.html',function (err,data) {
-          res.end(data);
-        });
-    }
-
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
